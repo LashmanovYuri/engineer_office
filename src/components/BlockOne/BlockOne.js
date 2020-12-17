353f0f51c243f7bcf7f33bb3aa997484bd2dd839
Fadin-47 <patriot-47@mail.ru>
2020-12-17 22:31:37 +0400

    ясделяль

--- src/components/BlockOne/BlockOne.js
+++ src/components/BlockOne/BlockOne.js
@@ -1,7 +1,8 @@
 // Импорт библиотек
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
+import { Doughnut } from 'react-chartjs-2';
 
 // Импорт действий Redux
 import {
@@ -25,30 +26,67 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(1),
   },
 }));
+//Диаграмма 
 
 
 // Функция-компонент
 function BlockOne({
 
 }) {
-  const classes = useStyles();
 
+  const classes = useStyles();
+  const [chartData, setChartData] = useState({})
+
+  const chart = () => {
+    setChartData({      
+      labels: ['1', '2', '3', '4', '5', '6'],
+      datasets: [
+        {          
+          data: [1, 2, 3, 4, 5, 6],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)',
+            'rgba(255, 159, 64, 0.7)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          
+        }],
+            
+    })
+  }
+
+
+
+
+  useEffect(() => {
+    chart()
+  }, [])
 
   return (
     <Card className={classes.root}>
       <Typography variant='h5'>
-        Блок - Захаров Антон
+        Блок - Фадин Алексей
       </Typography>
-      <IconButton
-        onClick={() => {
-          alert('Hello')
-        }}
-      >
-        <MailIcon />
-      </IconButton>
+      <Doughnut data={chartData} options={{cutoutPercentage:50, 
+                                    title: {text:'enginer diagram', display:true},
+                                    animation: {animateScale: true},                                    
+                                    }} />
+
+
     </Card>
   );
 }
+
 
 
 // Redux (не удалять!!!)
